5adff0a86c7611fd3d5d0ed6f177cb1a5598fcea
basharkhan7776 <basharkhan7776@gmail.com>
2025-04-17 22:46:43 +0530

    feat: initialize wallet adapter project with TypeScript, React, and Vite
    - Added ESLint configuration for TypeScript and React with recommended rules.
    - Created index.html as the entry point for the application.
    - Set up package.json with necessary dependencies and scripts for development, building, and linting.
    - Included Vite logo SVG in the public directory.
    - Created initial App component with basic styling.
    - Added React logo SVG to assets.
    - Configured Tailwind CSS in index.css.
    - Implemented main.tsx to render the App component.
    - Defined TypeScript environment types in vite-env.d.ts.
    - Configured TypeScript settings in tsconfig files for app and node.
    - Set up Vite configuration with React and Tailwind CSS plugins.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="h-screen w-screen bg-red-300 text-3xl font-bold flex items-center justify-center font-mono">
+      wwifowefonwoenf
+    </div>
+  )
+}
+
+export default App
