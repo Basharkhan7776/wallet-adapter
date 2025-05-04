f46dbb767eaf00478985113338a7e6d1ddc3a2a2
basharkhan7776 <basharkhan7776@gmail.com>
2025-05-04 22:11:18 +0530

    Add Shadcn and rewap ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletDisconnectButton, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { TokenLaunchpad } from '@/components/TokenLaunchpad';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+
 
 function App() {
-
   return (
-    <div className="h-screen w-screen bg-red-300 text-3xl font-bold flex items-center justify-center font-mono">
-      wwifowefonwoenf
+    <div className='flex py-10 px-44 flex-col justify-center items-center min-h-screen'>
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]}>
+          <WalletModalProvider>
+            <div>
+              <h1 className='text-4xl font-bold text-center font-mono'>Token Launchpad</h1>
+              <div className='w-full flex justify-end gap-4 items-center p-4'>
+                <WalletMultiButton />
+                <WalletDisconnectButton />
+              </div>
+            </div>
+            <TokenLaunchpad />
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </div>
   )
 }
+
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+    server: {
+      host: "::",
+      port: 3000,
+    },  
+  plugins: [react(), tailwindcss(), nodePolyfills()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
